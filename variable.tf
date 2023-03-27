7eacb3a2a288316d033b79fa4cff329e1a0fd847
anilp2005 <anilp2005@gmail.com>
2023-03-27 13:05:35 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIA45JDUNPMZWFC5PN2"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "ap-south-1"
+  }
